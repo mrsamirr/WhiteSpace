b9ad5b61940f5d97dee51ee1db12b3bdb4846742
Md Samer Ansari <smaitykhan@gmail.com>
2024-08-12 23:54:46 +0530

    init

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
 
@@ -8,17 +8,48 @@ export const bookRouter = new Hono<{
     Bindings: {
         DATABASE_URL: string,
         JWT_SECRET: string,
+    },
+     Variables: {
+       userId: string
     }
-}>()
+}>();
 
 
-  bookRouter.use('/*', async (c, next) => {
-    const response = verify(c.req.headers.auhtorization, c.env.JWT_SECRET)
+  bookRouter.use( async (c, next) => {
+    const jwt = c.req.header('Authorization');
+    if(!jwt) {
+      c.status(401);
+      c.json({ error: "Unauthorized" });
+    }
+    const token = jwt.split(' ')[1]; // Bearer cjbepihvcperihv
+    const payload = await verify(token, c.env.JWT_SECRET);
+    if(!payload) {
+      c.status(401);
+      c.json({ error: "Unauthorized" });
+    }
+    c.set('userId', payload.id as string);
+    await next();
 
   })
-  bookRouter.post('/', (c) => {
-    return c.text('signin route')
-  })
+  bookRouter.post('/', async (c) => {
+    const userId = c.get('userId');
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+        const post = await prisma.post.create({
+          data: {
+              title: body.title,
+              content: body.content,
+              authorId: userId
+          }
+        });
+    
+          return c.json({
+           id: post.id
+          })
+    })
   
   bookRouter.put('/', (c) => {
     return c.text('signin route!')
