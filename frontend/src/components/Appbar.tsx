9a09c514c17ae992e0cb817c633e657235a761b6
Md Samer Ansari <smaitykhan@gmail.com>
2024-08-25 21:06:51 +0530

    stsh

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -15,8 +15,8 @@ export const Appbar = () => {
         <div>
             <Link to={'/publish'}>
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">New Blog</button>
-            <Avatar name="Md Samerr Ansari" size={"big"} />
             </Link>
+            <Avatar name="User" size={"big"} />
             <Link
           onClick={logout}
           to='/signin'
@@ -27,3 +27,5 @@ export const Appbar = () => {
         </div>
     </div>
 }
+
+
